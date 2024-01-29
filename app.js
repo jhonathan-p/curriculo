178208cb3cf705f12d533620ec9821f56f58b8fa
Jhonathan Pfeiffer Urbainski <108313123+jhonathan-p@users.noreply.github.com>
2024-01-29 14:04:41 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-function toggleMode() {
+document.addEventListener('DOMContentLoaded', function () {
     let styleSheetLink = document.getElementById('styleSheet');
-    let currentTheme = document.documentElement.getAttribute('data-theme');
     let toggleButton = document.getElementById('toggleMode');
 
-    if (currentTheme === 'dark') {
-        document.documentElement.setAttribute('data-theme', 'light');
-        styleSheetLink.setAttribute('href', './styles/lightmode.css');
-        toggleButton.innerHTML = 'Modo<br>Escuro';
-        localStorage.setItem('theme', 'light');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        styleSheetLink.setAttribute('href', './styles/style.css');
-        toggleButton.innerHTML = 'Modo<br>Claro';
-        localStorage.setItem('theme', 'dark');
-    }
-}
+    function toggleMode() {
+        let currentHref = styleSheetLink.getAttribute('href');
 
-document.addEventListener('DOMContentLoaded', function () {
-    const savedTheme = localStorage.getItem('theme');
+        if (currentHref === './styles/style.css') {
+            styleSheetLink.setAttribute('href', './styles/lightmode.css');
+            toggleButton.innerHTML = 'Modo<br>Escuro';
+            localStorage.setItem('theme', 'light');
+        } else {
+            styleSheetLink.setAttribute('href', './styles/style.css');
+            toggleButton.innerHTML = 'Modo<br>Claro';
+            localStorage.setItem('theme', 'dark');
+        }
+    }
+
+    toggleButton.addEventListener('click', toggleMode);
+
+    let savedTheme = localStorage.getItem('theme');
     if (savedTheme) {
         document.documentElement.setAttribute('data-theme', savedTheme);
-        let styleSheetLink = document.getElementById('styleSheet');
-        let toggleButton = document.getElementById('toggleMode');
+
         if (savedTheme === 'light') {
             styleSheetLink.setAttribute('href', './styles/lightmode.css');
             toggleButton.innerHTML = 'Modo<br>Escuro';
